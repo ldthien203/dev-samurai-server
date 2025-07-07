3d8c31cec1121b2939129bd9768e4c1748e012b8
ldthien203 <96572606+ldthien203@users.noreply.github.com>
2025-07-07 15:38:13 +0700

    refactor getResponse

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,27 +1,17 @@
 import { Request, Response } from 'express'
-// import { successResponse, errorResponse } from '@/utils/ApiResponse.util'
+import { successResponse, errorResponse } from '@/utils/ApiResponse.util'
 
 const getResponse = (req: Request, res: Response) => {
   if (!req.user) {
-    res.status(401).json({
-      success: false,
-      message: 'Unauthorized',
-    })
+    return errorResponse(res, new Error('User not found'), 'Unauthorized', 401)
   } else {
-    res.status(200).json({
-      success: true,
-      message: 'API response successful',
-      data: {
-        name: req.user.name,
-        email: req.user.email,
-      },
-    })
+    return successResponse(
+      res,
+      { name: req.user.name, email: req.user.email },
+      'API response successful',
+      200
+    )
   }
-  // if (!req.user) {
-  //   return errorResponse(res, new Error('User not found'), 'Unauthorized', 401)
-  // } else {
-  //   return successResponse(res, req.user, 'API response successful', 200)
-  // }
 }
 
 export default getResponse
